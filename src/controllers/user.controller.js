514926def1d7f193aa0aa05ab4d7863ab319de0e
Pratap <pratap@gmail.com>
2026-01-18 22:21:55 +0530

    bug fixes and working with postman

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -20,7 +20,7 @@ const registerUser = asyncHandler(async (req, res)=> {
 
     // get user details from frontend
     const {fullName, email, username, password} = req.body
-    console.log("email: ", email);
+    // console.log("email: ", email);
 
 
      // validation - not empty
@@ -30,7 +30,7 @@ const registerUser = asyncHandler(async (req, res)=> {
 
 
     // check if user already exist: username, email
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -38,11 +38,18 @@ const registerUser = asyncHandler(async (req, res)=> {
         throw new ApiError(409, "User with email or username already existed")
     }
 
-
+    // console.log(req.files);
 
     // check for images, check for avatar
     const avatarLocalPath = req.files?.avatar[0].path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
+
+    
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required");
